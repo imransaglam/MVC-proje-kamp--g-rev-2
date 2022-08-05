359219765767f5c49972b9d7508cdacae0832ede
imransaglam <imransaglam9@gmail.com>
2022-08-05 14:45:25 +0300

    mvc-proje-kampı
    mvc-proje-kampı

---
+++ MvcProjeKampi/BusinessLayer/ValidationRules/CategoryValidatior.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidatior:AbstractValidator<Category>
+    {
+        public CategoryValidatior()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsin");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı boş geçemezsin");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("En az üç karakter girişi yapın");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("En fazla yirmi karakter girişi yapın");
+        }
+
+    }
+}
---
+++ MvcProjeKampi/MvcProjeKampi/Controllers/IstatistikController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context context = new Context();
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            var istatistik1 = context.Categories.Count();
+            var istatistik2 = context.Headings.Count(x => x.Category.CategoryID == 7);
+            var istatistik3 = context.Writers.Count(x => x.WriterName.Contains("a"));
+            var istatistik4 = context.Headings.Max(x => x.Category.CategoryName);
+            var istatistik5 = context.Categories.Count(x => x.CategoryStatus == true);
+            var istatistik6 = context.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.CategoryCount = istatistik1;
+            ViewBag.Heading = istatistik2;
+            ViewBag.Writer = istatistik3;
+            ViewBag.HeadingMax = istatistik4;
+            ViewBag.StatusFark = (istatistik5 - istatistik6);
+
+            return View();
+        }
+    }
+}
